da5fa7fa6cbce71a1c78bec83020779077d028ce
Garri3105 <nikola.novisad2@gmail.com>
2025-06-03 13:56:39 +0200

    Dodao dva controllera i dodao metodu u modelu trening

---
+++ CRUD/src/crud/controller/uvidUTreningIOdabir/UvidUTreningeIOdabirController.java
@@ -0,0 +1,63 @@
+package crud.controller.uvidUTreningIOdabir;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import crud.model.Clan;
+import crud.model.Trening;
+import crud.model.db.Baza;
+
+public class UvidUTreningeIOdabirController {
+	private UvidUTreningeIOdabirView view;
+	private Clan clan;
+	private Baza baza;
+	
+	public UvidUTreningeIOdabirController (UvidUTreningeIOdabirView view, Clan clan, Baza baza) {
+        this.view = view;
+        this.clan = clan;
+        this.baza = baza;
+
+        prikaziTreninge();
+    }
+	
+	private void prikaziTreninge() {
+		List<Trening> treninzi = this.getTreninzi();
+		view.prikaziTreninge(treninzi);
+	}
+	
+	public ArrayList<Trening> getTreninzi() {
+		ArrayList<Trening> sviTreninzi = baza.readAllTrening();
+		ArrayList<Trening> naredniTreninzi = new ArrayList<>();
+		
+		LocalDateTime trenutnoVreme = LocalDateTime.now();
+		LocalDateTime maxVreme = trenutnoVreme.plusDays(10);
+		
+		for (Trening t : sviTreninzi) {
+			if (t.getVremeTreninga().isAfter(trenutnoVreme) && t.getVremeTreninga().isBefore(maxVreme))
+				naredniTreninzi.add(t);
+		}
+		
+		return naredniTreninzi;
+	}
+	
+	public boolean rezervisiTrening(Trening trening) {
+		Trening selektovanTrening = view.getSelektovanTrening();
+		
+        if (trening.getBrojSlobodnihMesta() <= 0) {
+        	view.prikaziPoruku("Nema slobodnih mesta.");
+			return false;
+        }
+
+        if (trening.postojiClan(clan)) {
+        	view.prikaziPoruku("Vec ste prijavljeni na taj trening.");
+            return false;
+        }
+
+        trening.dodajClana(clan);
+        view.prikaziPoruku("Uspesno ste rezervisali.");
+        return true;
+    }
+	
+
+}
